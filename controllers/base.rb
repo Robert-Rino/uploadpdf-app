1ea1fad5be4362c2275c58cf266e8ae8cf3c36da
Robert-Rino <a901002666@gmail.com>
2016-09-19 14:22:17 +0800

    update controller

--- controllers/base.rb
+++ controllers/base.rb
@@ -1,9 +1,26 @@
 require 'sinatra'
+require 'rack-flash'
 
 # Base class for ShareFilesApp Web Application
 class UploadPdfApp < Sinatra::Base
+  enable :logging
+
+  use Rack::Session::Cookie, secret: ENV['MSG_KEY'],
+                             expire_after: 60 * 60 * 24 * 7
+  use Rack::Flash
+
+  configure :production do
+    use Rack::SslEnforcer
+  end
+
   set :views, File.expand_path('../../views', __FILE__)
   set :public_dir, File.expand_path('../../public', __FILE__)
+
+  before do
+    if session[:current_account]
+      @current_account = SecureMessage.decrypt(session[:current_account])
+    end
+  end
 
   get '/' do
     slim :home
